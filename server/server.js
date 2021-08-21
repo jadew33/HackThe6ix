cfbc5be4a42efec44b8398b14e3cc5cebb4245fc
jadew33 <77248838+jadew33@users.noreply.github.com>
2021-08-21 11:17:00 -0400

    merged frontend with backend

--- server/server.js
+++ server/server.js
@@ -2,14 +2,29 @@ const firebaseConfig = require("./config/config");
 const firebase = require("firebase");
 require("firebase/firestore");
 
-const goalRoutes = require("./routes/GoalRoutes");
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.json());
+app.use(cors());
+
 firebase.initializeApp(firebaseConfig);
 
-app.listen(5001, () => {
-  console.log("Listening on port 5001");
-});
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+
+app.listen(5001, () =>{
+    console.log("Listening on port 5001");
+})
